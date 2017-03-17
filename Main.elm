1878f2377e3eb3bbca2695e2cd33a022e2bfb383
chrisr <c@24.org.uk>
2017-03-17 19:08:08 +0000

    Split model into another module

--- Main.elm
+++ Main.elm
@@ -5,8 +5,8 @@ import Svg.Attributes exposing (..)
 import Svg.Events exposing (onClick)
 import Dict exposing (Dict)
 import Keyboard exposing (KeyCode, downs, presses)
-import Set
 import Char
+import Model exposing (Model, Position, Piece)
 
 main =
   Html.program
@@ -16,105 +16,8 @@ main =
     , subscriptions = subscriptions
     }
 
-
--- MODEL
-
-type alias Piece = { name: String, shape: Shape, position: Position}
-type alias Shape = {width : Int, height : Int, color: String}
-type alias Position = { r: Int, c: Int }
-type alias Model = {pieces: Dict String Piece, active: String}
-
-numRows = 5
-numCols = 4
-
-big: Shape
-big = {width = 2, height = 2, color = "crimson"}
-
-small: Shape
-small = {width = 1, height = 1, color = "green"}
-
-tall: Shape
-tall = {width = 1, height = 2, color = "teal"}
-
-wide: Shape
-wide = {width = 2, height = 1, color = "orange"}
-
-
 init : (Model, Cmd Msg)
-init =
-  ({pieces = Dict.fromList [
-    ("a", {name = "a", shape = big, position = {r = 0, c = 1}}),
-    ("b", {name = "b", shape = tall, position = {r = 0, c = 0}}),
-    ("c", {name = "c", shape = tall, position = {r = 0, c = 3}}),
-    ("d", {name = "d", shape = tall, position = {r = 2, c = 0}}),
-    ("e", {name = "e", shape = tall, position = {r = 2, c = 3}}),
-    ("f", {name = "f", shape = wide, position = {r = 2, c = 1}}),
-    ("g", {name = "g", shape = small, position = {r = 4, c = 0}}),
-    ("h", {name = "h", shape = small, position = {r = 3, c = 1}}),
-    ("i", {name = "i", shape = small, position = {r = 3, c = 2}}),
-    ("j", {name = "j", shape = small, position = {r = 4, c = 3}})
-  ], active = "a"}, Cmd.none)
-
-allPieces: Model -> List Piece
-allPieces model =
-  Dict.values model.pieces
-
-valid: Model -> Bool
-valid model =
-  inbounds model && noOverlaps model
-
-inbounds: Model -> Bool
-inbounds model =
-  let
-    positions = List.map (\piece -> piece.position) (allPieces model)
-    rows = List.map (\position -> position.r) positions
-    maxRow = Maybe.withDefault 0 (List.maximum rows)
-    minRow = Maybe.withDefault 0 (List.minimum rows)
-    columns = List.map (\position -> position.c )positions
-    maxCol = Maybe.withDefault 0 (List.maximum columns)
-    minCol = Maybe.withDefault 0 (List.minimum columns)
-  in
-    minRow >= 0 && maxRow < numRows && minCol >= 0 && maxCol < numCols
-
-
-noOverlaps: Model -> Bool
-noOverlaps model =
-  let
-    pieces = (allPieces model)
-    allCoverage = (List.map coverage pieces) |> List.concat
-    noDupes = dropDuplicates allCoverage
-  in
-    List.length allCoverage == List.length noDupes
-
-coverage: Piece -> List Position
-coverage piece =
-  let
-    r = piece.position.r
-    allRows = List.range r (piece.shape.height - 1 + r)
-    c = piece.position.c
-    allCols = List.range c (piece.shape.width - 1 + c)
-    tuples = cartesian allRows allCols
-  in
-    List.map (\(r,c) -> {r = r, c = c}) tuples
-
-cartesian : List a -> List b -> List (a,b)
-cartesian xs ys =
-  List.concatMap
-    ( \x -> List.map ( \y -> (x, y) ) ys )
-    xs
-
-dropDuplicates : List Position -> List Position
-dropDuplicates list =
-  let
-    step next (set, acc) =
-      let
-        hash = next.c + (next.r * 1000)
-      in
-        if Set.member hash set
-          then (set, acc)
-          else (Set.insert hash set, next::acc)
-  in
-    List.foldl step (Set.empty, []) list |> Tuple.second |> List.reverse
+init = (Model.initial, Cmd.none)
 
 -- UPDATE
 
@@ -158,7 +61,7 @@ makeMove model direction =
           movedPiece = {piece | position = newPosition}
           newModel = updatePiece model model.active movedPiece
         in
-          if valid newModel then newModel else model
+          if Model.valid newModel then newModel else model
       _  -> model
 
 updatePosition: Position -> Direction -> Position
@@ -210,7 +113,7 @@ renderBoard = [
 renderPieces: Model -> List (Svg Msg)
 renderPieces model =
   let
-    pieces = (allPieces model)
+    pieces = (Model.allPieces model)
   in
     List.map (renderPiece model.active) pieces
 
